d2e0fb8dde0618b00e5e0c5c02b4368e34bef918
Ankit65-cyber <ank71887@gmail.com>
2026-06-19 17:58:11 +0530

    Add files via upload

---
+++ exp4script.js
@@ -0,0 +1,8 @@
+// Page Loaded Message
+console.log("Responsive Website Loaded Successfully");
+
+// Welcome Message
+window.onload = function()
+{
+    alert("Welcome to My Responsive Website");
+};
